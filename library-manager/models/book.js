87277f4d1dc8539f3a45e7ecc1b8227ba8dcc6be
“Nicole <dicocheanicole@gmail.com>
2023-10-27 08:39:11 -0700

    add book model

---
+++ library-manager/models/book.js
@@ -0,0 +1,46 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Book extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Book.init({
+    title: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate:  {
+        notNull: {
+          msg: '"Title" cannot be blank'
+        }, 
+        notEmpty: {
+          msg: '"Title" cannot be blank'
+        }
+      }
+    },
+    author: Sequelize.STRING,
+    allowNull: false,
+    validate:  {
+      notNull: {
+        msg: '"Title" cannot be blank'
+      }, 
+      notEmpty: {
+        msg: '"Title" cannot be blank'
+      }
+    },
+    genre: DataTypes.STRING,
+    year: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Book',
+  });
+  return Book;
+};
