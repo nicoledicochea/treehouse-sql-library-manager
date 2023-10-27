95eb2bc45e46517627e8f84c457acb7712f16c92
“Nicole <dicocheanicole@gmail.com>
2023-10-27 10:59:37 -0700

    update templates/'

--- library-manager/routes/books.js
+++ library-manager/routes/books.js
@@ -2,19 +2,24 @@ const express = require('express');
 const router = express.Router();
 const Book = require('../models').Book;
 
+// show full list of books
 router.get('/', async(req, res) => {
     const books = await Book.findAll();
-    res.render('index',  { books })
+    res.render('index',  { books, title: "Books" })
 })
 
-router.get('/new', async(req, res) => {
+// show create new book form
+router.get('/new', (req, res) => {
     res.render('new-book')
 })
 
+// post new book to database
 router.post('/new', async(req, res) => {
-    //
+    const book = await Book.create(req.body)
+    res.redirect(`/books/${book.id}`)
 })
 
+// show individual book details
 router.get('/:id', async(req, res) => {
     const id = req.params.id
     const book = await Book.findByPk(id)
@@ -24,14 +29,18 @@ router.get('/:id', async(req, res) => {
 
 })
 
+// update info in database
 router.post(':id', async(req, res) => {
     const id = req.params.id
     const book = await Book.findByPk(id)
+    await book.update(req.body)
+    res.redirect(`/books/${book.id}`)
 })
-
+// delete a book
 router.post(':id/delete', async(req, res) => {
     const id = req.params.id
     const book = await Book.findByPk(id)
+    await book.destroy()
 })
 
 module.exports = router;
