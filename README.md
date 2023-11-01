14cff93a8578726d4ca5e26bb05c625875c59a3a
“Nicole <dicocheanicole@gmail.com>
2023-11-01 15:51:27 -0700

    update readme with project start instructions

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# treehouse-sql-library-manager
+# SQL Library Manager
+#### Run the following in the terminal to access the main folder and run the program.
+
+Navigate to the correct folder
+``` 
+cd library-manager/
+```
+Install dependencies
+``` 
+npm install
+```
+Start the  program
+```
+npm start  
+```
--- library-manager/routes/index.js
+++ library-manager/routes/index.js
@@ -1,20 +1,13 @@
 var express = require('express');
 var router = express.Router();
-const Book = require('../models').Book;
 
-/* GET home page. */
+// redirect home page to main books list
 router.get('/', async function(req, res, next) {
-  // test global error handler
+  // // test global error handler
   // const err = new Error()
   // err.status = 500
   // next(err)
-
-  // res.render('index', { title: 'Express' });
-  // const books = await Book.findAll();
-  // console.log(books)
-  // await res.json(books)
   res.redirect('/books')
-  
 });
 
 module.exports = router;
