6ab1782fc38c06724e0611b155ad1b0551d6559f
“Nicole <dicocheanicole@gmail.com>
2023-11-01 16:27:29 -0700

    cleanup app.js

--- library-manager/app.js
+++ library-manager/app.js
@@ -1,17 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const sequelize = require('./models').sequelize;
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var booksRouter = require('./routes/books');
-var searchRouter = require('./routes/search');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const booksRouter = require('./routes/books');
+const searchRouter = require('./routes/search');
 
-
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
